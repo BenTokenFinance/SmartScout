113064f6eceafc22260e4772e6ec04793206365d
ingmgni <ingmgni@github.com>
2024-04-16 14:18:56 +0800

    Add sh

---
+++ start.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+# ????docker-composeĿ¼
+cd docker-compose/ || exit
+
+# ??Զ?ֿ̲???ȡ???µĸ???
+git pull origin sbch
+
+# ????blockscout????
+# docker compose -f external-frontend.yml build
+
+# ??̨????blockscout????
+# FRONT_PROXY_PASS=http://host.docker.internal:3000 docker compose -f external-frontend.yml up --build -d
+# FRONT_PROXY_PASS=http://host.docker.internal:3000 docker compose -f external-frontend.yml up -d
+FRONT_PROXY_PASS=http://host.docker.internal:3000 docker compose up -d
+
+echo "Blockscout has been successfully updated and started."
